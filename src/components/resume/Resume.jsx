70eec7a7ac2046650ea4139a82e0f741d64e5db1
prathmeshj908 <106106397+prathmeshj908@users.noreply.github.com>
2024-01-08 20:12:46 +0530

    Add files via upload

---
+++ src/components/resume/Resume.jsx
@@ -0,0 +1,100 @@
+import React from "react";
+
+const Resume = () => {
+  return (
+    <div id="resume" className=" container m-auto   mt-16">
+      {/* heading */}
+      <div data-aos="fade-up" className="relative mb-5">
+        <h3 className=" text-3xl font-black text-gray-400 sm:text-2xl">
+          Education & Experience
+        </h3>
+        <span className="h-[1.1px] right-0 absolute w-[92%] bg-gray-300 block"></span>
+      </div>
+      <div data-aos="fade-up" className="left flex-1 w-full">
+        <p className=" text-gray-700 font-medium w-[100%]">
+          Here are my experiences and qualifications.
+        </p>
+      </div>
+      {/* card*/}
+      <div className="card-wrapper w-[90%] sm:w-full mt-5 flex md:flex-col sm:gap-5 mx-auto ">
+        <div className="left flex-1 flex items-center justify-center">
+          <fieldset
+            data-aos="zoom-in"
+            className=" w-[80%] p-5 py-12 sm:py-8 sm:w-full sm:p-2"
+          >
+            <legend className=" w-auto ml-[50%] translate-x-[-50%] border-2 border-gray-200 rounded-3xl py-1 px-8 font-semibold text-xl text-yellow-500">
+              Experience
+            </legend>
+            <div className=" relative">
+              {/* design */}
+              <div className="design flex absolute left-[-150px] top-1/2 items-center rotate-[90deg] sm:left-[-160px] ">
+                <div className="c1 w-[12px] h-[12px] rounded-full bg-white border-2 border-yellow-500"></div>
+                <div className="line w-[230px] bg-gray-300 h-[2px] sm:w-[250px]"></div>
+                <div className="c2 w-[12px] h-[12px] rounded-full bg-white border-2 border-yellow-500"></div>
+              </div>
+              {/* design */}
+              <div className=" flex flex-col gap-1 sm:gap-1 border-2 border-yellow-400 shadow-[0px_0px_16px_1px_rgba(0,0,0,0.1)] p-3 rounded-lg">
+                <h1 className="text-[1.4rem] font-semibold sm:text-xl">
+                  Software Developer Intern
+                </h1>
+                <span className=" text-[.9rem] font-semibold text-gray-500 sm:text-base">
+                  BitMap Technology
+                </span>
+                <span className=" text-[.9rem] font-semibold text-yellow-500 sm:text-base">
+                  November 2022 - April 2023
+                </span>
+                <p className=" text-[.9rem] text-justify break-words text-gray-500">
+                  I worked in BitMap Technology as JAVA Developer.
+                  Developed a feature-rich Android application using Java/Kotlin and Firebase.
+                  Integrated Firebase Realtime Database to manage and sync data in real-time.
+                  Implemented Firebase Authentication for secure user authentication.
+                  Collaborated with cross-functional teams to enhance app
+                  functionality and user experience.
+                </p>
+              </div>
+            </div>
+          </fieldset>
+        </div>
+        <div className="right flex-1 flex items-center justify-center">
+          <fieldset
+            data-aos="zoom-in"
+            className=" w-[80%] p-5 py-12 sm:py-8 sm:w-full sm:p-2"
+          >
+            <legend className=" w-auto ml-[50%] translate-x-[-50%] border-2 border-gray-200 rounded-3xl py-1 px-8 font-semibold text-xl text-yellow-500">
+              Education
+            </legend>
+            <div className=" relative">
+              {/* design */}
+              <div className="design flex absolute left-[-150px] top-1/2 items-center rotate-[90deg] sm:left-[-160px] ">
+                <div className="c1 w-[12px] h-[12px] rounded-full bg-white border-2 border-yellow-500"></div>
+                <div className="line w-[230px] bg-gray-300 h-[2px] sm:w-[250px]"></div>
+                <div className="c2 w-[12px] h-[12px] rounded-full bg-white border-2 border-yellow-500"></div>
+              </div>
+              {/* design */}
+              <div className=" flex flex-col gap-1 border-2 border-yellow-400 shadow-[0px_0px_16px_1px_rgba(0,0,0,0.1)] p-3 rounded-lg">
+                <h1 className="text-[1.4rem] font-semibold sm:text-xl">
+                  B.E, Computer Science
+                </h1>
+                <span className=" text-[.9rem] font-semibold text-gray-500 sm:text-base">
+                  Sinhgd Academy of Engineering
+                </span>
+                <span className=" text-[.9rem] font-semibold text-yellow-500 sm:text-base">
+                  Year 2019 - Year 2023
+                </span>
+                <p className=" text-[.9rem] text-justify text-gray-500">
+                  I completed my Engineering at Sinhgad Academy of Engineering Kondhawa.
+                  I have good understanding of C++, Data Structers and Algorithm.
+                  Additionaly I am Proficient in technologies as Machine Learning,
+                  HTML, CSS, Javascript, Nodejs. I have skill in Python Django and Flask.
+
+                </p>
+              </div>
+            </div>
+          </fieldset>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Resume;
---
+++ src/data/data.js
@@ -0,0 +1,30 @@
+
+
+export const data = [
+ 
+  {
+    id: 1,
+    img: 'https://i.postimg.cc/Bv75CLyN/4167022.jpg',
+    title: "Tarvel Agency",
+    link: "https://travelo-website.vercel.app",
+    git: "https://github.com/prathmeshj908",
+  },
+  {
+    id: 2,
+    img:"https://i.postimg.cc/pTsFRkw1/Cartoonify.jpg",
+    
+    title: "Cartoonify image Web",
+    desc: "",
+    link: "",
+    git: "https://github.com/prathmeshj908/Cartoonify-Image",
+  },
+  {
+    id: 3,
+    img: 'https://i.postimg.cc/wBjcJcW7/5890625.jpg',
+    title: "Music Player",
+    desc: "",
+    link: "https://music-player-delta-ecru.vercel.app",
+    git: "https://github.com/prathmeshj908/Music-Player",
+  }
+  
+];
